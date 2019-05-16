50c1e25fbb7306962b9de52701e75d3e8da48cfa
teastonsmith2 <t.easton.smith2@gmail.com>
2019-05-16 14:28:45 -0600

    first commit

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
-const express = require("express");
+require('dotenv').config()
+
+const express = require('express');
+const massive = require('massive');
+
+const { CONNECTION_STRING } = process.env
 
 const app = express();
 
@@ -7,5 +12,9 @@ const { SERVER_PORT } = process.env;
 app.use(express.json());
 
 app.listen(SERVER_PORT, () => {
-  console.log(`Server listening on port ${SERVER_PORT}`);
+	console.log(`Server listening on port ${SERVER_PORT}`);
 });
+
+massive(CONNECTION_STRING).then(dbInstance => {
+	app.set('db', dbInstance)
+})
